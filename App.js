aa156424ad8fd4b9a80cce815a2a1a015a59105c
Renaud <fournet.renaud@gmail.com>
2022-02-16 16:09:53 +0100

    fixed

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container1}>
+        <Text style={styles.blueText}>Hello, world!</Text>
+        <Text style={styles.blueText}>Hello, world!</Text>
+        <Text style={styles.blueText}>Hello, world!</Text>
+        <Button title={'Good'} />
+      </View>
+      <View style={styles.containerBottom}>
+        <Text style={styles.blueText}>Hello, world!</Text>
+        <Text style={styles.blueText}>Hello, world!</Text>
+        <Text style={styles.blueText}>Hello, world!</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'grey'
   },
-});
+  container1: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  containerBottom: {
+    alignItems: 'flex-end',
+    backgroundColor: 'green',
+    justifyContent: 'center'
+  },
+  blueText: { color: 'blue' }
+})
